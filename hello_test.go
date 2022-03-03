a601f662f1a9ae9905f74eb36f474aca9cb45a30
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2022-03-03 17:54:51 +0100

    initial commit

---
+++ hello_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("says 'Hello, World' when name is empty", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+}
