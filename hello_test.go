faed3d79d67490ef162563c5dd659efc05d1e2ee
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2022-03-03 18:59:14 +0100

    fixed spec

--- hello_test.go
+++ hello_test.go
@@ -29,7 +29,7 @@ func TestHello(t *testing.T) {
 	})
 
 	t.Run("in French", func(t *testing.T) {
-		got := Hello("Elodie", "Frenc")
+		got := Hello("Elodie", "French")
 		want := "Bonjour, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
