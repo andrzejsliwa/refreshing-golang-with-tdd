4d4a73fa9ed7fed39289025e96d0d182ef69347e
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2022-03-03 18:58:39 +0100

    Small progress

--- hello_test.go
+++ hello_test.go
@@ -5,22 +5,32 @@ import "testing"
 func TestHello(t *testing.T) {
 	assertCorrectMessage := func(t *testing.T, got, want string) {
 		if got != want {
+			t.Helper()
 			t.Errorf("got %q want %q", got, want)
 		}
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "English")
 		want := "Hello, Chris"
-
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("says 'Hello, World' when name is empty", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World"
-
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "Frenc")
+		want := "Bonjour, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
 }
